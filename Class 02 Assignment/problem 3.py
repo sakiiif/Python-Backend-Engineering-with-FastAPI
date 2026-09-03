8f8d7d6e3587b549c0df02d95adb44dffcc17eb0
= <=>
2026-09-04 02:17:39 +0600

    problem 3 done

---
+++ Class 02 Assignment/problem 3.py
@@ -0,0 +1,10 @@
+numbers = [10, 20, 30, 40, 50, 60, 70]
+
+# First three elements
+print(numbers[:3])
+
+# Elements from index 2 to 4
+print(numbers[2:5])
+
+# from start to end jump by +2
+print(numbers[::2])
