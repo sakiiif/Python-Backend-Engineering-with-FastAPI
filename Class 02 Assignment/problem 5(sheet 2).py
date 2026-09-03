cbbb6dec837aa22cba282b18e810ed7137186a05
= <=>
2026-09-04 02:22:42 +0600

    problem 5 done

---
+++ Class 02 Assignment/problem 5(sheet 2).py
@@ -0,0 +1,8 @@
+numbers = [10, 20, 30, 40, 50]
+
+number = int(input())
+
+if number in numbers:
+    print("Yes, the number is in the list.")
+else:
+    print("No, the number is not in the list.")
